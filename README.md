a684d2cfa2ea262f1317a2114c11fc097675cdf3
dhazra <dhazra@tenant-ac-goodfre-login-0.tenant-ac-goodfre-login.tenant-ac-goodfre.svc.cluster.local>
2025-04-15 18:11:25 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# r1
-Open source interpretability artefacts for R1.
+# Open-source SAEs for DeepSeek-R1
+
+[[`Blog`]()] [[`Models`](https://huggingface.co/Goodfire/DeepSeek-R1-SAE-l37)]
+[[`Dataset`](https://huggingface.co/Goodfire/r1-collect)] [[`Feature
+Database`](https://huggingface.co/Goodfire/r1-collect)]
+
+We're open-sourcing [two state-of-the-art
+SAEs](https://huggingface.co/Goodfire/DeepSeek-R1-SAE-l37) trained on the 671B
+parameter DeepSeek R1. These are the **first public interpreter models** trained
+on a true reasoning model, and on **any model of this scale.** Because R1 is a
+very large model and therefore difficult to run for most independent
+researchers, we're also uploading SQL databases containing the max activating
+examples for each feature.
+
+We're excited to see how the wider research community
+will use these tools to develop new techniques for understanding and aligning
+powerful AI systems. As reasoning models continue to grow in capability and
+adoption, tools like these will be essential for ensuring they remain reliable,
+transparent, and aligned with human intentions. 
+
+## Model Information
+
+This release contains two SAEs, one for general reasoning and one for math, both
+of which are [available on
+HuggingFace](https://huggingface.co/Goodfire/DeepSeek-R1-SAE-l37). Load them
+with the following snippet:
+
+```python
+from sae import load_math_sae
+from huggingface_hub import hf_hub_download
+
+file_path = hf_hub_download(
+    repo_id=f"Goodfire/DeepSeek-R1-SAE-l37",
+    filename=f"math/DeepSeek-R1-SAE-l37.pt",
+    repo_type="model"
+)
+device = "cpu"
+math_sae = load_math_sae(file_path, device)
+```
+
+See `db_example.ipynb` for an example of how to interact with the database with
+max activating examples for each feature. 
---
+++ sae.py
@@ -0,0 +1,74 @@
+import torch
+from math import prod
+
+
+class BatchTopKTiedSAE(torch.nn.Module):
+    def __init__(
+        self,
+        d_in,
+        d_hidden,
+        k,
+        device,
+        dtype,
+        normalization_constant: int = 1,
+        tiebreaker_epsilon: float = 1e-6,
+    ):
+        super().__init__()
+        self.d_in = d_in
+        self.d_hidden = d_hidden
+        self.k = k
+        self.norm_constant = normalization_constant
+        W_mat = torch.randn((d_in, d_hidden))
+        W_mat = 0.1 * W_mat / torch.linalg.norm(W_mat, dim=0, ord=2, keepdim=True)
+        self.W = torch.nn.Parameter(W_mat)
+        self.b_enc = torch.nn.Parameter(torch.zeros(self.d_hidden))
+        self.b_dec = torch.nn.Parameter(torch.zeros(self.d_in))
+        self.device = device
+        self.dtype = dtype
+        self.tiebreaker_epsilon = tiebreaker_epsilon
+        self.tiebreaker = torch.linspace(0, tiebreaker_epsilon, d_hidden)
+        self.to(self.device, self.dtype)
+
+    def encoder_pre(self, x):
+        return x @ self.W + self.b_enc
+
+    def encode(self, x, tiebreak=False):
+        f = torch.nn.functional.relu(self.encoder_pre(x))
+        return self._batch_topk(f, self.k, tiebreak=tiebreak)
+
+    def _batch_topk(self, f, k, tiebreak=False):
+        if tiebreak:  # break ties in feature order for determinism
+            f += self.tiebreaker.broadcast_to(f)
+        *input_shape, _ = (
+            f.shape
+        )  # handle higher-dim tensors (e.g. from sequence input)
+        numel = k * prod(input_shape)
+        f_topk = torch.topk(f.flatten(), numel, dim=-1)
+        f_topk = (
+            torch.zeros_like(f.flatten())
+            .scatter(-1, f_topk.indices, f_topk.values)
+            .reshape(f.shape)
+        )
+        return f_topk
+
+    def decode(self, f):
+        return f @ self.W.T + self.b_dec
+
+    def forward(self, x):
+        x = x * self.norm_constant
+        f = self.encode(x)
+        return self.decode(f), f
+
+
+def load_math_sae(file_path, device: str = "cpu"):
+    state_dict = torch.load(file_path, weights_only=True, map_location=device)
+    sae = BatchTopKTiedSAE(d_in=7168, d_hidden=7168 * 4, dtype=torch.bfloat16, k=128)
+    sae.load_state_dict(state_dict)
+    return sae
+
+
+def load_logic_sae(file_path, device: str = "cpu"):
+    state_dict = torch.load(file_path, weights_only=True, map_location=device)
+    sae = BatchTopKTiedSAE(d_in=7168, d_hidden=7168 * 4, dtype=torch.bfloat16, k=64)
+    sae.load_state_dict(state_dict)
+    return sae
